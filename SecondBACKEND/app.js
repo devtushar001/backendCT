3e2d817adb35b4a9262807bcd3383a3e62cdaea5
devtushar001 <tc341269@gmail.com>
2025-02-05 23:53:39 -0800

    sdfsdf

--- SecondBACKEND/app.js
+++ SecondBACKEND/app.js
@@ -4,19 +4,19 @@ const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end("This is home page")
   }
-  
+
   if (req.url === '/about') {
     res.end("This is about page")
   }
-  
+
   if (req.url === '/profile') {
     res.end("This is profile page")
   }
 
-  if(req.url === '/') {
+  if (req.url === '/') {
     res.end('You got nothing!!')
   }
-  
+
 });
 
 server.listen(3000);
