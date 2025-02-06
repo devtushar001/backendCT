a96992793e237095196db2ab475fd88c683d7e89
devtushar001 <tc341269@gmail.com>
2025-02-05 23:02:20 -0800

    Tomorrows backend code

--- SecondBACKEND/app.js
+++ SecondBACKEND/app.js
@@ -1,19 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(`Server running`);
+  console.log(req.url)
   if (req.url === '/') {
     res.end("This is home page")
   }
-
+  
   if (req.url === '/about') {
     res.end("This is about page")
   }
-
+  
   if (req.url === '/profile') {
     res.end("This is profile page")
   }
-
+  
 });
 
 server.listen(3000);
